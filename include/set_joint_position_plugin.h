625ac8487773ff57fdac052baeab7099eac5e7b0
Nicolas Sommer <n.sommer@epfl.ch>
2016-04-04 13:15:04 +0200

    first commit

---
+++ include/set_joint_position_plugin.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2012-2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+/*
+ * Desc: A dynamic controller plugin that performs generic force interface.
+ * Author: John Hsu
+ * Date: 24 Sept 2008
+ */
+
+#ifndef MY_ROBOT_PLUGIN_HH
+#define MY_ROBOT_PLUGIN_HH
+
+#include <string>
+
+// Custom Callback Queue
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <sensor_msgs/JointState.h>
+
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/TransportTypes.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Events.hh>
+// #include <RobotLib/RobotLib.h>
+
+#include "gazebo/transport/transport.hh"
+
+
+
+
+namespace gazebo
+{
+
+
+class set_joint_position_plugin : public ModelPlugin
+{
+    /// \brief Constructor
+public: set_joint_position_plugin();
+
+    /// \brief Destructor
+public: virtual ~set_joint_position_plugin();
+
+    // Documentation inherited
+protected: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+    // Documentation inherited
+protected: virtual void UpdateChild();
+
+    /// \brief call back when a Wrench message is published
+    /// \param[in] _msg The Incoming ROS message representing the new force to exert.
+private: void CallBackMethod(const sensor_msgs::JointState _msg);
+
+    /// \brief The custom callback queue thread function.
+private: void QueueThread();
+
+    /// \brief A pointer to the ROS node.  A node will be instantiated if it does not exist.
+private: ros::NodeHandle* rosnode_;
+private: ros::Subscriber sub_;
+    sensor_msgs::JointState joint_state_;
+
+private: void PublishJointState();
+
+    /// \brief A mutex to lock access to fields that are used in ROS message callbacks
+private: boost::mutex lock_;
+
+    /// \brief ROS JointState topic name inputs
+private: std::string topic_name_;
+
+    /// \brief for setting ROS name space
+private: std::string robot_namespace_;
+
+    // Custom Callback Queue
+private: ros::CallbackQueue queue_;
+    /// \brief Thead object for the running callback Thread.
+private: boost::thread callback_queue_thread_;
+    // Pointer to the update event connection
+private: event::ConnectionPtr update_connection_;
+
+
+private: std::vector<physics::JointPtr> joints_list;
+
+private: sensor_msgs::JointState set_joint_state_;
+
+
+
+};
+}
+#endif
---
+++ src/set_joint_position_plugin.cpp
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2013 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+/*
+   Desc: my_robot_plugin plugin for manipulating objects in Gazebo
+   Author: John Hsu
+   Date: 24 Sept 2008
+ */
+
+#include <algorithm>
+#include <assert.h>
+
+#include <set_joint_position_plugin.h>
+
+namespace gazebo {
+GZ_REGISTER_MODEL_PLUGIN(set_joint_position_plugin);
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+set_joint_position_plugin::set_joint_position_plugin()
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Destructor
+set_joint_position_plugin::~set_joint_position_plugin()
+{
+  event::Events::DisconnectWorldUpdateBegin(this->update_connection_);
+
+  // Custom Callback Queue
+  this->queue_.clear();
+  this->queue_.disable();
+  this->rosnode_->shutdown();
+  this->callback_queue_thread_.join();
+  this->joints_list.clear();
+  this->sub_.shutdown();
+
+
+  delete this->rosnode_;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the controller
+void set_joint_position_plugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+  // load parameters
+  this->robot_namespace_ = "";
+
+  if ( _sdf->HasElement("robotNamespace") ) this->robot_namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>() + "/";
+
+
+  if ( !_sdf->HasElement("topicName") )
+  {
+    ROS_FATAL("force plugin missing <topicName>, cannot proceed");
+    return;
+  }
+  else this->topic_name_ = _sdf->GetElement("topicName")->Get<std::string>();
+
+
+  // Make sure the ROS node for Gazebo has already been initialized
+  if ( !ros::isInitialized() )
+  {
+    ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+                     << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+    return;
+  }
+
+  ROS_INFO_STREAM("topicName:" << this->topic_name_);
+  ROS_INFO_STREAM("Loading plugin for setting joint positions !:");
+
+  this->rosnode_ = new ros::NodeHandle(this->robot_namespace_);
+
+
+  this->sub_ = this->rosnode_->subscribe( this->topic_name_, 1, &set_joint_position_plugin::CallBackMethod, this, ros::TransportHints().tcpNoDelay() );
+
+
+  // Custom Callback Queue
+  this->callback_queue_thread_ = boost::thread( boost::bind(&set_joint_position_plugin::QueueThread, this) );
+
+  // New Mechanism for Updating every World Cycle
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+    boost::bind(&set_joint_position_plugin::UpdateChild, this) );
+
+
+  /* Checking the list of joints */
+  this->joints_list = _model->GetJoints();
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void set_joint_position_plugin::CallBackMethod(const sensor_msgs::JointState _msg)
+{
+  this->lock_.lock();
+  ROS_INFO_STREAM( "Getting a new position" );
+  set_joint_state_ =  _msg;
+  this->lock_.unlock();
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void set_joint_position_plugin::UpdateChild()
+{
+  this->lock_.lock();
+
+  for (int i = 0; i < set_joint_state_.position.size();i++){
+     #if GAZEBO_MAJOR_VERSION == 2
+        this->joints_list[i]->SetAngle(0, set_joint_state_.position[i]);   
+    #else // if GAZEBO_MAJOR_VERSION != 2
+        this->joints_list[i]->SetPosition(0, set_joint_state_.position[i]);
+    #endif // if GAZEBO_MAJOR_VERSION == 2
+  }
+
+  this->lock_.unlock();
+
+}
+
+// Custom Callback Queue
+////////////////////////////////////////////////////////////////////////////////
+// custom callback queue thread
+void set_joint_position_plugin::QueueThread()
+{
+  static const double timeout = 0.01;
+
+  while ( this->rosnode_->ok() )
+  {
+    this->queue_.callAvailable( ros::WallDuration(timeout) );
+  }
+}
+
+
+}
